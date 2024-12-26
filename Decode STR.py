bb37a9dbf929704ec97342645cfebd9b400c624c
547Varghese <varghesegeorge.2008@gmail.com>
2024-12-26 18:57:00 +0530

    Decode STR.py

--- Decode STR.py
+++ Decode STR.py
@@ -2,22 +2,22 @@
 
 
 
-##Simple code
-'''
+
+
 s=input('String : ')
 v=''
 for i in s:
     v+=chr(ord(i)-1)
 print(v)
 
+
+
+
+
+
+
+##Trial
 '''
-
-
-
-
-
-
-
 d={'a':'z','z':'y','y':'x','x':'w','w':'v','v':'u','u':'t','t':'s','s':'r','r':'q','q':'p','p':'o','o':'n','n':'m','m':'l','l':'k','k':'j','j':'i','i':'h','h':'g','g':'f','f':'e','e':'d','d':'c','c':'b','b':'a'}
 D={'A': 'Z', 'Z': 'Y', 'Y': 'X', 'X': 'W', 'W': 'V', 'V': 'U', 'U': 'T', 'T': 'S', 'S': 'R', 'R': 'Q', 'Q': 'P', 'P': 'O', 'O': 'N', 'N': 'M', 'M': 'L', 'L': 'K', 'K': 'J', 'J': 'I', 'I': 'H', 'H': 'G', 'G': 'F', 'F': 'E', 'E': 'D', 'D': 'C', 'C': 'B', 'B': 'A'} 
 S=input('String : ')
@@ -28,3 +28,7 @@ for i in range(len(S)):
     else:
         A+=d[S[i]]
 print(A)
+'''
+
+
+
