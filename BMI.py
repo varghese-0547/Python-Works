50f234ff6d1919645132a64e3c147c25ba6b2ad1
Varghese George <varghesegeorge.2008@gmail.com>
2025-01-01 23:17:35 +0530

    BMI.py

--- BMI.py
+++ BMI.py
@@ -1,12 +1,37 @@
-W=float(input("Enter your wiaght in kilogram : "))
-H=float(input("Enter your height in meter : "))
-H/=100
-BMI=W/(H**2)
-if BMI<18.5:
-    print("Your BMI is",BMI,"\nCategory : Underweight")
-elif 18.5 <= BMI < 24.9:
-    print("Your BMI is",BMI,"\nCategory : Normal")
-elif 24.9 <= BMI < 30:
-    print("Your BMI is",BMI,"\nCategory : Overweight")
-elif BMI > 30:
-    print("Your BMI is",BMI,"\nCategory : Obesity")
+while True:
+    W=float(input("Enter your weight in kilogram : "))
+    H=float(input("Enter your height in centi meter : "))
+    H/=100
+    BMI=W/(H**2)
+    print(" ")
+    if BMI<18.5:
+        print("Your BMI is",BMI,"\nCategory : Underweight")
+        print('''\nUnderweight = <18.5
+Normal weight = 18.5–24.9
+Overweight = 25–29.9
+Obesity = BMI of 30 or greater''')
+        break
+    elif 18.5 <= BMI < 25.0:
+        print("Your BMI is",BMI,"\nCategory : Normal")
+        print('''\nUnderweight = <18.5
+Normal weight = 18.5–24.9
+Overweight = 25–29.9
+Obesity = BMI of 30 or greater''')
+        break
+    elif 25.0 <= BMI < 30:
+        print("Your BMI is",BMI,"\nCategory : Overweight")
+        print('''\nUnderweight = <18.5
+Normal weight = 18.5–24.9
+Overweight = 25–29.9
+Obesity = BMI of 30 or greater''')
+        break
+    elif 41>BMI >= 30:
+        print("Your BMI is",BMI,"\nCategory : Obesity")
+        print('''\nUnderweight = <18.5
+Normal weight = 18.5–24.9
+Overweight = 25–29.9
+Obesity = BMI of 30 or greater''')
+        break
+    else:
+        print("\nSorry, there is an error.\nEnter one more time\n")
+        continue
