ef17ea0621a34a9e2d11880163e026c0c5c84a63
Varghese George <varghesegeorge.2008@gmail.com>
2025-07-03 22:35:21 +0530

    Fn.py

--- Fn.py
+++ Fn.py
@@ -1,20 +1,56 @@
-def Fees():
+def Add():
     import mysql.connector as mc
     con=mc.connect(host='localhost',user='root',passwd='root',database='EDU_School')
     cur=con.cursor
-    q='select admno, name, class, fees from T1 where admno={}'.format(_ADMNO)
+    _n=input('Name :')
+    _dob=input('DOB :')
+    _pn=input("Parent's name:")
+    _pjob=input("Parent's job")
+    _ph=int(input("Phno :"))
+    _class
+    n='select count(adnmo) from eduschool'
+    cur.execute(n)
+    nn=cur.fetchall()
+    _rs=Addfees()
+    q=f"insert into eduschool values(f'{nn+1001},'{_n}','{ _dob}', '{_pn}', '{_pjob}', {_ph} ,{_class})"
+    q1=f"insert into FeeTable values({_admno},'{_n}', {_class},{_rs} )"
     cur.execute(q)
-    rs=cur.fetchall()
-    print(rs)
+    cur.execute(q1)
+    con.commit()
     cur.close()
     con.close()
 ##########################################################################################
 
-def Trnspt():
+def Login():
     import mysql.connector as mc
     con=mc.connect(host='localhost',user='root',passwd='root',database='EDU_School')
     cur=con.cursor
-    q='select * from T1 where admno={}'.format(_ADMNO)
+    q='select * from eduschool where admno={}'.format(_ADMNO)
+    cur.execute(q)
+    rs=cur.fetchall()
+    cur.close()
+    con.close()
+    if _Admno not in rs:
+        s='no'
+    elif _Admno in rs:
+        print(f'Welcome {rs[1]}')
+##########################################################################################
+def Addfees():
+    
+    if _class<=3:
+        _fees=15000
+    elif _class<=6:
+        _fees=20000
+    elif _class<=10:
+        _fees=25000
+    return _fees
+##########################################################################################
+
+def Fees():
+    import mysql.connector as mc
+    con=mc.connect(host='localhost',user='root',passwd='root',database='EDU_School')
+    cur=con.cursor
+    q='select admno, name, class, fees from FeeTable where admno={}'.format(_ADMNO)
     cur.execute(q)
     rs=cur.fetchall()
     print(rs)
@@ -37,35 +73,36 @@ def Results():
     d1 = today.strftime("%d/%m/%Y")
     print("No results have been published till",d1)
 ##########################################################################################
-def Add():
+def Edit():
     import mysql.connector as mc
     con=mc.connect(host='localhost',user='root',passwd='root',database='EDU_School')
     cur=con.cursor
-    _n=input('Name :')
-    _dob=input('DOB :')
-    _fn=input("Father's name:")
-    _fjob=input("Father's job")
-    _ph=int(input("Phno :"))
-    
-    n='select count(adnmo) from eduschool'
-    cur.execute(n)
-    nn=cur.fetchall()
-    q=f"insert into T1 values(f'{nn+1001},{_n},{ _dob}, {_fn}, {_fjob}, {_ph} )"
-    cur.execute(q)
-    con.commit()
-    cur.close()
-    con.close()
-##########################################################################################
-def Login():
-    import mysql.connector as mc
-    con=mc.connect(host='localhost',user='root',passwd='root',database='EDU_School')
-    cur=con.cursor
-    q='select * from T1 where admno={}'.format(_ADMNO)
-    cur.execute(q)
-    rs=cur.fetchall()
-    cur.close()
-    con.close()
-    if _Admno not in rs:
-        s='no'
-    elif _Admno in rs:
-        print(f'Welcome {rs[1]}')
+    if _ch==1:
+        a=input("Enter name :")
+        q="update eduschool set name = '{a}' where admno = {_Admno}")
+        cur.execute(q)
+        con.commite()
+    elif _ch==2:
+        a=input("Enter new DOB :")
+        q="update eduschool set name = '{a}' where admno = {_Admno}")
+        cur.execute(q)
+        con.commite()
+    elif _ch==3:
+        a=input("Enter Father name:")
+        q="update eduschool set name = '{a}' where admno = {_Admno}")
+        cur.execute(q)
+        con.commite()
+    elif _ch==4:
+        a=input("Enter Father job:")
+        q="update eduschool set name = '{a}' where admno = {_Admno}")
+        cur.execute(q)
+        con.commite()
+    elif _ch==5:
+        a=input("Enter new Phno :")
+        q="update eduschool set name = '{a}' where admno = {_Admno}")
+        cur.execute(q)
+        con.commite()
+
+
+
+
