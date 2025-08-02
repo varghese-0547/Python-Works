97a3684742b439477c16875d2096108f383e12a1
Varghese George <varghesegeorge.2008@gmail.com>
2025-08-03 00:05:49 +0530

    MFn.py

--- MFn.py
+++ MFn.py
@@ -1,11 +1,12 @@
 from pickle import *
-stu=[[1001, 'Jeyendu', '29-03-2008', 'Jayaram', 9446454105, 10, 25000], [1002, 'Mariya', '29-03-2008', 'John', 9446454105, 10, 25000],
+'''stu=[[1001, 'Jeyendu', '29-03-2008', 'Jayaram', 9446454105, 10, 25000], [1002, 'Mariya', '29-03-2008', 'John', 9446454105, 10, 25000],
 [1003, 'Feba', '20-11-2008', 'Reji Paul', 9037078758, 10, 25000], [1004, 'Pooja', '29-03-2008', 'MG Muralidharan', 9496397607, 10, 25000]]
+
 fw=open("Rec.dat","wb")
 dump(stu,fw)
 fw.close()
 print("Data added successfully")
-
+'''
 #   In write mode, the close() is not for saving the file but to move the file pointer back to (0,0)
 
 def Add(_class,_fees):
@@ -38,7 +39,7 @@ def Add(_class,_fees):
     F=_fees
     print("If you enter any of your record incorrectly just submission")
     print("Go to the editting window to edit the records")
-    s=input("Do you wish to continue submission :")
+    s=input("Do you wish to continue submission y/n:")
     if s in ['y','Y','yes','Yes','YES']:
         data=[_rno,_n,_dob,_pn,_ph,c,F]
         
@@ -47,24 +48,26 @@ def Add(_class,_fees):
         with open("Rec.dat","wb") as fw:
             dump(stu,fw)
         print("Data added successfully ")
+    else:
+        print("No records were added")
         
-#Add(10,25000)
+#Add()
 
 ##########################################################################################
 
-def Login():
+def Login(_Admno):
     with open("Rec.dat","rb") as fr:
         stu=load(fr)
         m=0
-        r=int(input("Enter Roll no. :"))
+        
         L=[]
         for i in stu:
             L.append(i[0])
-            if i[0]==r:
+            if i[0]==_Admno:
                 m=1
-                print("Welcome",i[1])
+                return i[1]
         if m==0:
-            print("Record not found")
+            return 0
 #Login()
      
 ##########################################################################################
@@ -77,56 +80,72 @@ def Display():
         print(i[0],i[1])
     fr.close()
 #Display()
+##########################################################################################
+def DisplayAll():
+    fr=open("Rec.dat","rb")
+    stu=load(fr)
+    print()
+    for i in stu:
+        print(i)
+    fr.close()
+
+
 
 
 ##########################################################################################
-#   If you call this fn() it will initially erase all data, so /Beware
+
 def Edit():
-    print("*****Edit*****\n")
-    Display()
+    print("\n*****Edit*****\n")
     fr=open("Rec.dat","rb")
     stu=load(fr)
-    ft=open("Rectemp.dat","wb")
-    dump(stu,ft)
-    fw=open("Rec.dat","wb")
-    c=input("Do you wish to continue :")
-    if c in ['y','Y','yes','Yes','YES'] :
-        r=int(input("Enter Roll no :"))
-        for i in stu:
-            if i[0]==r:
-                print(i)
-        print("1 Edit Name")
-        print("2 Edit DOB")
-        print("3 Edit Father Name")
-        print("4 Phno")
-        print("5 Edit Class")
-        _ch=int(input("Enter choice :"))
-
-        for i in stu:
-            if i[0]==r:
-
-                if _ch==1:
-                    a=input("Enter name :")
-                    i[1]=a
-                elif _ch==2:
-                    a=input("Enter new DOB :")
-                    i[2]=a
-                elif _ch==3:
-                    a=input("Enter Father name:")
-                    i[3]=a
-                elif _ch==4:
-                    a=int(input("Enter Phno :"))
-                    i[4]=a
-                elif _ch==5:
-                    a=int(input("Enter Class :"))
-                    i[5]=a
-                break
-        print("Successfully updated")
-    dump(stu,fw)
     fr.close()
-    ft.close()
-    fw.close()
-Edit()
+    Display()
+    r=int(input("\nEnter Admno :"))
+    for i in stu:
+        if i[0]==r:
+            print(i)
+    print("1 Edit Name")
+    print("2 Edit DOB")
+    print("3 Edit Father Name")
+    print("4 Phno")
+    print("5 Edit Class")
+    print("6 Edit Fees\n")
+    _ch=int(input("Enter choice :"))
+
+    for i in stu:
+        if i[0]==r:
+            if _ch==1:
+                a=input("Enter name :")
+                i[1]=a
+            elif _ch==2:
+                a=input("Enter new DOB :")
+                i[2]=a
+            elif _ch==3:
+                a=input("Enter Father name:")
+                i[3]=a
+            elif _ch==4:
+                a=int(input("Enter Phno :"))
+                i[4]=a
+            elif _ch==5:
+                a=int(input("Enter Class :"))
+                i[5]=a
+            elif _ch==6:
+                a=int(input("Enter Fees :"))
+                i[6]=a
+
+            s=input("Do you wish to continue submission y/n:")
+            if s in ['y','Y','yes','Yes','YES']:
+    
+                fw=open("Rec.dat","wb")
+                dump(stu,fw)
+                fw.close()
+                print("Successfully updated")
+                break
+    else:
+        print("No updates were done")
+
+
+#Edit()
     
 ##########################################################################################
 def Addfees(_class):
@@ -139,8 +158,13 @@ def Addfees(_class):
     return _fees
 ##########################################################################################
 
-def Fees():
-    print(_fees)
+def Fees(_Admno):
+    fr=open("Rec.dat","rb")
+    stu=load(fr)
+    for i in stu:
+        if i[0]==_Admno:
+            print(i[6])
+    fr.close()
 ##########################################################################################
 
 def Projects():
