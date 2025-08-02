727dd82f9a836f771301998585bad8a371c89107
Varghese George <varghesegeorge.2008@gmail.com>
2025-08-03 00:07:41 +0530

    Main.py

--- Main.py
+++ Main.py
@@ -1,52 +1,45 @@
 from MFn import *
 while True:
- 
+    
     print("\n===============================EDU Public School===============================")
 
-    print("1 = Admission\n2 = Login")
-    ch=int(input("Please enter your choice :"))
-    if ch==1:
+    print("1 = Admission\n2 = Login\n3 = Display all records\n4 = Edit Records")
+    ch=input("Please enter your choice :")
+
+    if ch=='1':
         print("Select the class",1,2,3,4,5,6,7,8,9,10)
         _class= int(input("Enter the class : "))
-        if _class<=10:
-            try:
-                Add()
-            except ModuleNotFoundError:
-                print("Please install MySQL and import the required lib")
-                continue
+        if _class in range(1,11):
+            _fees=Addfees(_class)
+            Add(_class,_fees)
+        else:
+            print("Invalid Class\Try again")
 
-    if ch==2:
-        _Admno=input("Enter your admission number :")
-        s=Login(_Admno)
-        
-        if s=='NO':
+    if ch=='2':
+        _Admno=int(input("Enter your admission number :"))
+        p=Login(_Admno)
+        if p==0:
             print("Invalid Admission number\nTry again")
             continue
-        print("\nWelcome Mariyam\n")
-        print("1 = Fees\n2 = Transportation\n3 = Projects\n4 = Edit Profile")
-        ch=int(input("Please enter your choice :"))
-        
-        if ch==1:
-            Fees()
+        print("\nWelcome", p)
+        print("1 = Fees\n3 = Projects\n3 = Edit Profile")
+        _ch=input("Please enter your choice :")
+        if _ch=='1':
+            Fees(_Admno)
 
-        elif ch==3:
+        elif _ch=='2':
             Projects()
+        else:
+            print("Invalid choice")
+    if ch=='3':
+        DisplayAll()
 
-        elif ch==4:
-            print("You can modify the following data")
-            print("1 = Student name\n2 = DOB\n3 = Father name\n4 = Father job\n5 = Phno")
-            _ch=int(input("Enter choice :"))
-            Edit()
-
-
-
-
-
-
-
-
-
-
+    if ch=='4':
+        Edit()
+    C=input("\n\nDo you wish to continue y/n:")
+    if C in ['n','N','No','no',]:
+        print("Have a nice day")
+        break
 
 
 
