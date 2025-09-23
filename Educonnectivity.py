65be7c311a4d915c602b6929e314fafe0909004d
Varghese George <varghesegeorge.2008@gmail.com>
2025-09-23 07:15:02 +0530

    Update Educonnectivity.py

--- Educonnectivity.py
+++ Educonnectivity.py
@@ -20,7 +20,7 @@ while True:
             print('''\n\n0 = Log Out
 1 = Admission
 2 = Display records
-3 = Display fee records
+3 = Search in records
 4 = Edit Records
 5=Delete Records''')
             ch=input("Please enter your choice :")
@@ -34,20 +34,41 @@ while True:
                         print("Please install MySQL and import the required lib")
                         continue
             elif ch=='2':
-                print("\n1. Display all records\n2. Sort by Class\n3. Sort by Date of Admission")
+                print("\n1. Display all records\n2. Sort by Class\n3. Sort by Date of Birth\n4. Sort by Date of Admission")
                 op=input("Choice :")
                 if op == '1':
                     Display()
                 elif op == '2':
-                    Display('B')
+                    Display('Class')
                 elif op =='3':
-                    Display('C')
+                    Display('DOB')
+                elif op=='4':
+                    Display('S_DOA')
             elif ch=='3':
-                print("Fn on progress")
-            elif ch=='4':
+                print('''\n1. Search by Admno\n2. Search by Name\n3. Search by DOB
+4. Search by Parent Name\n5. Search by Parent Job\n6. Search by Phone no\n7.Search by Class
+8. Search by DOA''')
+                op=input("Choice :")
+                if op == '1':
+                    Search()
+                elif op == '2':
+                    Search("Name")
+                elif op =='3':
+                    Search("DOB")
+                elif op=='4':
+                    Search("PN")
+                elif op=='5':
+                    Search("PJ")
+                elif op=='6':
+                    Search("Ph")
+                elif op=='7':
+                    Search("Class")
+                elif op=='8':
+                    Search()
+            elif ch=="4":
                 User=int(input("\nEnter Admission number of\nthe record to be editted :"))
                 Edit(User)
-            elif ch=='5':
+            elif ch=="5":
                 User=int(input("\nEnter Admission number of\nthe record to be deleted :"))
                 Delete(User)
             elif ch=='0':
