a49ee6e38cf886dd9c8bea7a463a7ee3274b0beb
Varghese George <varghesegeorge.2008@gmail.com>
2025-09-24 19:34:28 +0530

    Update Educonnectivity.py

--- Educonnectivity.py
+++ Educonnectivity.py
@@ -1,8 +1,6 @@
 from Fnconnectivity import *
 while True:
-    
     print("\n===============================EDU Public School===============================")
-
     print("1 = Admin Login\n2 = Parent Login")
     c=input("Enter choice :")
     if c=='1':
@@ -12,79 +10,143 @@ while True:
             continue
         else:
             pass
-    
-            
-            
         print("\n===============================  Host  ===============================")
         while True:
-            print('''\n\n0 = Log Out
+            print("_"*55)
+            print("-"*85)
+            print('''\n0 = Log Out
 1 = Admission
 2 = Display records
 3 = Search in records
 4 = Edit Records
 5=Delete Records''')
-            ch=input("Please enter your choice :")
+            ch=input("\nPlease enter your choice :")
             if ch=='1':
-                print("Select the class",1,2,3,4,5,6,7,8,9,10)
-                Class= int(input("Enter the class : "))
-                if Class<=10:
-                    try:
+                while True:
+                    print("-"*85)
+                    print("\nSelect the class",1,2,3,4,5,6,7,8,9,10)
+                    Class=input("\nEnter the class : ")
+                    if Class in ['1','2','3','4','5','6','7','8','9','10']:
+                        Class=int(Class)
                         Add(Class)
-                    except ModuleNotFoundError:
-                        print("Please install MySQL and import the required lib")
-                        continue
-            elif ch=='2':
-                print("\n1. Display all records\n2. Sort by Class\n3. Sort by Date of Birth\n4. Sort by Date of Admission")
-                op=input("Choice :")
-                if op == '1':
-                    Display()
-                elif op == '2':
-                    Display('Class')
-                elif op =='3':
-                    Display('DOB')
-                elif op=='4':
-                    Display('S_DOA')
-            elif ch=='3':
+                        break
+                    elif Class=='0' or Class in ['exit','Exit','EXIT']:
+                        break
+                    else :
+                        print("Sorry choose a valid option")
+            elif ch=='2':#Display
+                print("-"*85)
+                print('''\n1. Display all records\n2. Sort by Class
+3. Sort by Date of Birth\n4. Sort by Date of Admission''')
+                while True:
+                    print("-"*85)
+                    op=input("\nChoice :")
+                    if op == '1':
+                        Display()
+                        break
+                    elif op == '2':
+                        Display('Class')
+                        break
+                    elif op =='3':
+                        Display('DOB')
+                        break
+                    elif op=='4':
+                        Display('S_DOA')
+                        break
+                    elif op=='0' or op in ['exit','Exit','EXIT']:
+                        break
+                    else:
+                        print("Sorry choose a valid option")
+            elif ch=='3':#Search
                 print('''\n1. Search by Admno\n2. Search by Name\n3. Search by DOB
-4. Search by Parent Name\n5. Search by Parent Job\n6. Search by Phone no\n7.Search by Class
-8. Search by DOA''')
-                op=input("Choice :")
-                if op == '1':
-                    Search()
-                elif op == '2':
-                    Search("Name")
-                elif op =='3':
-                    Search("DOB")
-                elif op=='4':
-                    Search("PN")
-                elif op=='5':
-                    Search("PJ")
-                elif op=='6':
-                    Search("Ph")
-                elif op=='7':
-                    Search("Class")
-                elif op=='8':
-                    Search()
-            elif ch=="4":
-                User=int(input("\nEnter Admission number of\nthe record to be editted :"))
-                Edit(User)
-            elif ch=="5":
-                User=int(input("\nEnter Admission number of\nthe record to be deleted :"))
-                Delete(User)
+4. Search by Parent Name\n5. Search by Parent Job\n6. Search by Phone no
+7.Search by Class\n8. Search by DOA''')
+                while True:
+                    print("-"*85)
+                    op=input("\nChoice :")
+                    if op == '1':
+                        Search()
+                        break
+                    elif op == '2':
+                        Search("Name")
+                        break
+                    elif op =='3':
+                        Search("DOB")
+                        break
+                    elif op=='4':
+                        Search("PN")
+                        break
+                    elif op=='5':
+                        Search("PJ")
+                        break
+                    elif op=='6':
+                        Search("Ph")
+                        break
+                    elif op=='7':
+                        Search("Class")
+                        break
+                    elif op=='8':
+                        Search()
+                        break
+                    elif op=='0' or op in ['exit','Exit','EXIT']:
+                        break
+                    else:
+                        print("Sorry, choose a valid option")
+                        
+            elif ch=="4":#Edit
+                print("-"*85)
+                while True:
+                    User=input("\nEnter Admission number of\nthe record to be editted :")
+                    if User.isdigit():
+                        User=int(User)
+                        break
+                    if User=='0' or User in ['exit','Exit','EXIT']:
+                        break
+                    else:
+                        print("Sorry, enter a valid Admno")
+                        continue
+                    Edit(User)
+                    break
+            elif ch=="5":#Delete
+                print("-"*85)
+                while True:
+                    User=int(input("\nEnter Admission number of\nthe record to be deleted :"))
+                    if User.isdigit():
+                        User=int(User)
+                        
+                        break
+                    if User=='0' or User in ['exit','Exit','EXIT']:
+                        break
+                    else:
+                        print("Sorry, enter a valid Admno")
+                        continue
+                    Delete(User)
+                    break
             elif ch=='0':
                 break
-            
-
-    if c=='2':
-        User=int(input("Enter your admission number :"))
-        I=Login(User)
-        if I is None:
-            print("Try again\n")
-            continue
+    if c=='2':#Login
+        I=None
         while True:
+            User=input("\nEnter your admission number :")
+            if User=='0' or User in ['exit','Exit','EXIT']:
+                break
+            if User.isdigit():
+                User=int(User)
+                   
+            else :
+                print("Enter a valid Admno")
+                continue
+            I=Login(User)
+            if I is None or I=="None":
+                print("Try again")
+                break
+            
+        #while True:
             print("\n=============================== Parent ===============================\n")
             print("Welcome",I)
-            print("\n\n0 = Log Out\n1 = Fees\n2 = Projects\n3 = Display your details")
+            print("_"*55)
+            print("-"*85)
+            print("\n0 = Log Out\n1 = Fees\n2 = Projects\n3 = Display your details")
             ch=input("Please enter your choice :")
             if ch=='1':
                   Fees(User)
@@ -92,7 +154,7 @@ while True:
                  Projects()
             elif ch=='3':
                Display(User)
-            elif ch=='0':
+            elif ch=='0' or ch in ['exit','Exit','EXIT']:
                 break
             
     if c=='0':
